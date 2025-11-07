202a50acfb690dbeebb3e6ac7bdfbab3aa6aba2e
Avani <avanisharma2405@gmail.com>
2025-11-07 11:38:35 +0530

    first commit

---
+++ Basics/type_conversion.py
@@ -0,0 +1,31 @@
+# 1. input num as a string. You need to typecast into an integer and double it. 
+num = input()
+print(int(num)*2)
+
+# 2. Convert an integer a = 10 to float, string, and bool — print all.
+a = 10 
+print(a)
+print(float(a))
+print(str(a))
+print(bool(a))
+
+# 3. Convert a string "123" into integer and float and print both results.
+s ="123"
+print(int(s))
+print(float(s))
+
+# 4. Convert 0 and 1 into boolean and print the results.
+x = 0
+y = 1
+print(bool(x))
+print(bool(y))
+
+# 5. Convert a float value 3.14 into int and print the result.
+pi = 3.14
+print(int(pi))
+
+# 6. Convert True and False into integers and print both.
+b = True
+c = False 
+print(int(b))
+print(int(c))
