0bc3e6c7ddf58fa3409141694162c14d7381f5a4
Avani <avanisharma2405@gmail.com>
2025-11-13 09:02:26 +0530

    first commit

--- Basics/operators.py
+++ Basics/operators.py
@@ -9,6 +9,7 @@ t = x%y
 u = x//y
 v = x**y
 print(p, q, r, s, t, u, v)
+print()
 
 # 2. Logical operators
 a = int(input("enter a: "))
@@ -17,17 +18,51 @@ p = a and b
 q = a or b
 r = not a
 print(p, q, r)
+print()
 
-# 3. Bitwise operator
-a=int(input("enter a: "))
-b=int(input("enter b: "))
-c=int(input("enter c: "))
-d= a^a
-e= c^b
-f= a&b
-g= c|(a^a)
-e= ~e
-print(d, e, f, g)
+# 3. assignment operators
+a = 9
+a +=5
+print(a)
+a-=5
+print(a)
+a**=5
+print(a)
+print()
+
+# 4. comparison/relational operators
+a = 4
+b = 9
+print(a==b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a!=b)
+print()
+
+# 5. Bitwise operators
+print(6&3)
+print(6|3)
+print(6^3)
+print(6<<3)
+print(6>>3)
+print(~3)
+print()
+
+# 6. identiity operator
+x = [1,2, 3]
+y = [3,5 ,6]
+print(x is y)
+print(x==y)
+print(x is not y)
+print()
+
+# 7. membership operator
+a = [1,3,5,6]
+print("7" in a)
+print("9" not in a)
+
 
 
 
