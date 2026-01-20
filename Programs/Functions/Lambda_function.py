c637ec9634ac42ab073108d6f87ef9c37d2305fb
Avani <avanisharma2405@gmail.com>
2026-01-20 14:14:48 +0530

    first commit

---
+++ Programs/Functions/Lambda_function.py
@@ -0,0 +1,12 @@
+# defining the lambda function:
+# lambda function : it's a function which is used into the python for one line function....
+# it is define or initialized using the 'lambda' keyword
+# it is an anonymous function which is defined using the lambda keyword
+# syntax: lambda parameters: return
+
+# here we have stored the lambda function into the sum_1 variable
+sum_1 = lambda a, b:a+b
+
+# call sum_1 variable 
+print(sum_1(3, 5))
+
