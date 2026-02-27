5c5fc70c91b4d4c385021ccbcdb5758dcd227dab
Avani <avanisharma2405@gmail.com>
2026-02-27 11:25:50 +0530

    programs

--- Programs/Sets/basic_set.py
+++ Programs/Sets/basic_set.py
@@ -1,6 +1,7 @@
 # set: is a collection of unique elements which stores the element into the unordered manner 
 # it stores the heterogenous elements
 # unordered collection 
+# set is mutable: after creation we can make changes in it 
 
 # initialize the set: use {} braces
 s = {1,2, 'avani', 1,2}
@@ -15,3 +16,14 @@ print(type(s))
 s = set()
 print(type(s))
 
+# creating set from list: use set constructor
+li = [1,2,3,4]
+s = set(li)
+print(s)
+
+# in set we store immutable data types
+# if we store mutable data type then it gives error : unhashable type: 'list'
+s = {1,2, 3,4}
+print(s)
+
+# 
