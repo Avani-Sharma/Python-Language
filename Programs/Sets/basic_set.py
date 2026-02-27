cafb20a230629d2f4e6587493fe6774fd6074cfe
Avani <avanisharma2405@gmail.com>
2026-02-27 11:19:18 +0530

    basics of set

---
+++ Programs/Sets/basic_set.py
@@ -0,0 +1,17 @@
+# set: is a collection of unique elements which stores the element into the unordered manner 
+# it stores the heterogenous elements
+# unordered collection 
+
+# initialize the set: use {} braces
+s = {1,2, 'avani', 1,2}
+print(s)
+print(type(s))
+
+# we can't initialize empty set with the curly braces
+s = {}        # it gives dict 
+print(type(s))
+
+# to initialize the empty set: use set constructor
+s = set()
+print(type(s))
+
