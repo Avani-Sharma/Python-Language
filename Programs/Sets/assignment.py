1b7dff7011613ad0205e7b088edbb093b5f86406
Avani <avanisharma2405@gmail.com>
2026-03-02 22:47:30 +0530

    programs

---
+++ Programs/Sets/assignment.py
@@ -0,0 +1,24 @@
+# 1. create a set of numbers from 1 to 5 and print it.
+a = {1,'avani', 3, 4.7, 2}
+print("A: ", a)
+
+# 2. add an element to an existing set.
+b = {1,2,3,4}
+b.add(6)
+print("B: ", b)
+
+# 3. remove an element using remove().
+c = {1,2,3,4,5}
+c.remove(1)
+print("C: ", c)
+
+# 4. remove an element using discard().
+d = {1,2,3,4,5}
+result = d.discard(50)
+print("D: ", result)
+
+# 5. find the length of set
+e = {1,2,3,5,6,7,9}
+print("E: ", len(e))
+
+# 6. check if element exist in a set
--- Programs/String/indexing.py
+++ Programs/String/indexing.py
@@ -1,7 +1,29 @@
-s = "Avani Sharma"
-print(s[0])
-print(s[5])
-print(s[6])
-print(s[7])
-print(s[-5])
-print(s[-2])
+# string indexing
+
+# 1. Print the character at index 0 of the string "python".
+a = "python"
+print("A: ", a[0])
+
+# 2. Print the last character of "python" using negative indexing.
+b = "python"
+print("B:", b[-1])
+
+# 3. Print the character at index 4 of "programming".
+c = "programming"
+print("C:", c[4])
+
+# 4. Print the character at index -2 of "hello".
+d = "hello"
+print("D:", d[-2])
+
+# 5. Take a string from the user and print its first character.
+e = input()
+print("E:", e[0])
+
+# 6. Take a string from the user and print its last character.
+f = input()
+print("F:", f[-1])
+
+# 7. Print the middle character of "DataScience".
+g = "DataScience"
+print("G:", g[len(g)//2])
