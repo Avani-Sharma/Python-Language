a87e4c69076fbb5f0225acb3edee574c08bcfa94
Avani <avanisharma2405@gmail.com>
2026-03-03 16:52:13 +0530

    programs

--- Programs/Sets/assignment.py
+++ Programs/Sets/assignment.py
@@ -22,3 +22,29 @@ e = {1,2,3,5,6,7,9}
 print("E: ", len(e))
 
 # 6. check if element exist in a set
+f = {1,2,3,4,5}
+found = False
+for ele in f:
+    if ele == 4:
+        found = True
+        break
+print("F:", ele)
+
+# 7. Clear all elements from a set.
+g = {1,2,3,4,5}
+g.clear()
+print("G:", g)
+
+# 8. Convert a list with duplicate values into a set to remove duplicates.
+h = [1,3,2,3,4,2,4]
+unique_set = set(h)
+print("H:", unique_set)
+
+# 9. Create an empty set correctly (without using {}).
+i = set()
+print("I:", type(i))
+
+# 10. Iterate through a set and print each element.
+j = {1,2,3,4,5}
+for ele in j:
+    print("J:", ele)
--- Programs/String/check_string.py
+++
@@ -1,3 +0,0 @@
-txt = "the best thing in life are free"
-print("free" in txt)
-print("good" in txt)
