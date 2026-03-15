92e765a44b69e697dea95a8e22975810f3bdd613
Avani <avanisharma2405@gmail.com>
2026-03-16 04:38:11 +0530

    programs

--- Programs/Patterns/hollow_square_pattern.py
+++ Programs/Patterns/hollow_square_pattern.py
@@ -2,7 +2,7 @@
 for i  in range(1, 5):
     for j in range(1, 5):
         if (i==1 or i==4 or j==1 or j==4):
-            print(i, end=" ")
+            print(j, end=" ")
         else:
             print(" ", end=" ")
     print()
@@ -16,4 +16,15 @@ for i  in range(1, 5):
             print("*", end=" ")
         else:
             print(" ", end=" ")
+    print()
+
+# ques3
+for i  in range(1, 5):
+    ch = 'A'
+    for j in range(1, 5):
+        if (i==1 or i==4 or j==1 or j==4):
+            print(ch, end=" ")
+        else:
+            print(" ", end=" ")
+        ch = chr(ord(ch)+1)
     print()
--- Programs/Patterns/sandglass_pattern.py
+++ Programs/Patterns/sandglass_pattern.py
@@ -66,3 +66,25 @@ for i in range(2, 5):
         else:
             print(" ", end=" ")
     print()
+
+print()
+
+# number sandglass pattern
+for i in range(4, 0, -1):
+  for j in range(4, 0, -1):
+    if j<=i:
+      print("*", end=" ")
+    else:
+      print(" ", end=" ")
+  for j in range(2, i+1):
+    print("*", end=" ")
+  print()
+for i in range(2, 5):
+  for j in range(4, 0, -1):
+    if j<=i:
+      print("*", end=" ")
+    else:
+      print(" ", end=" ")
+  for j in range(2, i+1):
+    print("*", end=" ")
+  print()
