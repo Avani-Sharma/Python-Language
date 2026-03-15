7268c61fea438d017b3afd2a92904aa61e599ce9
Avani <avanisharma2405@gmail.com>
2026-03-15 18:32:04 +0530

    butterfly pattern

---
+++ Programs/Patterns/butterfly_pattern.py
@@ -0,0 +1,26 @@
+# butterfly pattern
+for i in range(1, 5):
+    for j in range(1, 5):
+        if j<=i:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    for j in range(4, 0, -1):
+        if j<=i:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+for i in range(3, 0, -1):
+    for j in range(1, 5):
+        if j<=i:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    for j in range(4, 0, -1):
+        if j<=i:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
