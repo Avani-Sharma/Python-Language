2cb868907f73b2da9ffc67a2beb25ccc56e32a34
Avani <avanisharma2405@gmail.com>
2026-03-25 09:49:48 +0530

    programs

---
+++ Programs/OOPS/Encapsulation.py
@@ -0,0 +1,2 @@
+# encapsulation: a fundamental concept of Object-Oriented Programming (OOP) that involves bundling data 
+# (attributes) and methods that operate on that data into a single unit, typically a class.
