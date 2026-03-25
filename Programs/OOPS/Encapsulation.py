df31607fb0bf0f9a42aac6ea57a2ad4c09d756d1
Avani <avanisharma2405@gmail.com>
2026-03-25 10:32:48 +0530

    programs

--- Programs/OOPS/Encapsulation.py
+++ Programs/OOPS/Encapsulation.py
@@ -1,2 +1,48 @@
 # encapsulation: a fundamental concept of Object-Oriented Programming (OOP) that involves bundling data 
 # (attributes) and methods that operate on that data into a single unit, typically a class.
+
+# or 
+
+# encapsulation is the way to wrap the data (variable) into the single unit (single class) so, it's 
+# access could be restricted.
+
+# private attributes: are the attributes which is not accessible outsideof the class.
+# private attributes are initialized with double underscores '__'.
+# ex: bank balance, password, etc.
+
+# Marks Management System
+class Student:
+    def __init__(self, marks, name, id):
+        # private attribute
+        self.__marks = marks
+        self.name = name
+        self.id = id
+obj = Student(40, 'avani', 'abcd')
+print(obj.name)
+print(obj.id)
+
+
+# protected attribute: initialized with one underscore '_'.
+class ATM:
+    def __init__(self, pin, name, balance):
+        # private attributes
+        self.__pin = pin 
+        # public attributes
+        self.name = name 
+        # protected attributes
+        self._balance = balance 
+    # getter method is used to get the values of the private and protected attribute of the class.
+    # this display method is accessing the protected attribute 
+    def display_balance(self):
+        balance = self._balance
+        print(f'the balance is: {balance}')
+    # to change the pin of atm card
+    # this change pin is setter method which is used to set or change or update the value of 
+    # private attribute.
+    def change_pin(self, new_pin):
+        self.__pin = new_pin
+        print("pin change successfully")
+# creating the pin of the card
+obj = ATM(1234, 'avani', 50000)
+obj.display_balance()
+obj.change_pin(9876)
