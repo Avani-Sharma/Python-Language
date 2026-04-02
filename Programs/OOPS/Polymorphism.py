86147d7dcba41267c674f399b1a53a39eaea2144
Avani <avanisharma2405@gmail.com>
2026-04-02 18:14:14 +0530

    programs

--- Programs/OOPS/Polymorphism.py
+++ Programs/OOPS/Polymorphism.py
@@ -1,5 +1,18 @@
 # Polymorphism: poly means many and morphism means forms 
 # it is one of the most important pillar of OOPS. in which a function or method have many forms
 # there are two types of polymorphism: 
-# 1. compile time polymorphism (method overloading)
-# 2. run time polymorphism (method overriding)
+# 1. compile time polymorphism (method overloading): feature where a class has multiple methods with 
+# the same name but different parameters(numbers, type or order). 
+# why python doesn't support method overloading: due to its dynamic nature and the way it manages name.
+# like name overwrite.
+
+# 2. run time polymorphism (method overriding): where a subclass provide its own specific implementation
+# of a method that is already defined in its parent class. 
+class Animal:
+    def make_sound(self):
+        print("animal makes sound")
+class Dog(Animal):
+    def make_sound(self):
+        print("barks")
+obj = Dog()
+obj.make_sound()
