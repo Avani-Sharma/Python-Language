6ed5bb4508c6af3a67d46761999b5f365ce8bb61
Avani <avanisharma2405@gmail.com>
2026-04-02 17:50:30 +0530

    programs

---
+++ Programs/OOPS/Polymorphism.py
@@ -0,0 +1,5 @@
+# Polymorphism: poly means many and morphism means forms 
+# it is one of the most important pillar of OOPS. in which a function or method have many forms
+# there are two types of polymorphism: 
+# 1. compile time polymorphism (method overloading)
+# 2. run time polymorphism (method overriding)
