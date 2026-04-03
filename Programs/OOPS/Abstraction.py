23d7cf8de96df9d90c3a56d92e62645a536c183a
Avani <avanisharma2405@gmail.com>
2026-04-03 10:42:46 +0530

    programs

--- Programs/OOPS/Abstraction.py
+++ Programs/OOPS/Abstraction.py
@@ -5,3 +5,31 @@
 # a common interface that all subclasses must follow, ensuring they implement specific methods.
 # what is abstract base class: in python abc stands for abstract base class. It is a module in python standard 
 # library used to define blueprint for other.
+# Enforced Interfaces: If a class inherits from an ABC and does not implement all of its "abstract methods," 
+# Python will raise a TypeError when you try to create an instance of that class.
+
+# example
+# here we have imported the ABC(helper class) and abstract method the module abc.
+from abc import ABC, abstractmethod
+# here we have created a vehicle class (interface or abstract base class)
+class Vehicle(ABC):
+    @abstractmethod
+    def start(self):
+        pass
+    @abstractmethod
+    def stop(self):
+        pass
+class EvCar(Vehicle):
+    def start(self):
+        print("the car starts!")
+    def stop(self):
+        print("the car stop!")
+class PetrolCar(Vehicle):
+    def start(self):
+        print("petrol car starts!")
+    def stop(self):
+        print("car Stop")
+obj = EvCar()
+obj1 = PetrolCar()
+obj.start()
+obj1.start()
