d4546b9bb5ce80d574a698db58df790e87c67b57
Avani <avanisharma2405@gmail.com>
2026-04-15 10:06:37 +0530

    programs

---
+++ Programs/File Handling/basic.py
@@ -0,0 +1,6 @@
+# file handling: is the process of the handling file or storing the data into the permanent storage.
+
+# file: file stores the data like audio, video, text
+# files are of two types: 
+# 1. text file(human readable files)
+# 2. binary file (0 & 1)
