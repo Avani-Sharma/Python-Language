52c6fed315a011d1ce0653ea4a330ee2dc70fab8
Avani <avanisharma2405@gmail.com>
2026-04-15 10:34:53 +0530

    programs

--- Programs/File Handling/basic.py
+++ Programs/File Handling/basic.py
@@ -3,4 +3,22 @@
 # file: file stores the data like audio, video, text
 # files are of two types: 
 # 1. text file(human readable files)
-# 2. binary file (0 & 1)
+# 2. binary file (0 & 1) (non-readable)
+
+# file read: here we're opening the file into the read mode. it means the script only access to read the file.
+# mode = access 
+# syntax: open('filepath', 'mode')
+
+# readlines : this function returns the list of all the lines which is stored into the file 
+# read function : returns all the content of file into the form of text
+# readline: this function returns one by one line. one line at a time.
+# example
+file = open('file.txt', 'r')
+content = file.read()
+content1 = file.readline()
+content2 = file.readlines()
+print(content)
+print(content1)
+print(content2)
+# after performing all the operations on the file we have to close that file by using the .close method
+file.close()
