5daae28de7c0f22f72ecc4bb7dfc4a91fac89667
Avani <avanisharma2405@gmail.com>
2026-04-20 06:22:11 +0530

    programs

--- Leetcode Questions/two_sum.py
+++ Leetcode Questions/two_sum.py
@@ -1,15 +1,15 @@
-li = [3,2,4]
-target = 6
-arr = [(num, i) for i, num in enumerate(li)]
-arr.sort()
-left = 0
-right = len(arr)-1
-while left<right:
-    s = arr[left][0] + arr[right][0]
-    if s==target:
-        print([arr[left][1], arr[right][1]])
-        break
-    elif s<target:
-        left +=1
-    else:
-        right -=1
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        arr = [(num, i) for i, num in enumerate(nums)]
+        arr.sort()
+        left = 0
+        right = len(arr)-1
+        while left<right:
+            s = arr[left][0] + arr[right][0]
+            if s==target:
+                return [arr[left][1], arr[right][1]]
+            elif s<target:
+                left +=1
+            else:
+                right -=1
+        return -1
